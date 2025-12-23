a2abff2ada318794a059e50a7cb8abb4f76dc18b
Andrea-FEVE <85485500+Andrea-FEVE@users.noreply.github.com>
2025-12-23 21:58:50 +0100

    Update project.js

--- api/project.js
+++ api/project.js
@@ -1,1 +1,17 @@
+export default async function handler(req, res) {
+  if (!(req.headers.cookie || "").includes("auth=1"))
+    return res.status(401).end();
+
+  const { slug } = req.query;
+
+  const r = await fetch(
+    `https://api.airtable.com/v0/${process.env.BASE_ID}/Projects?filterByFormula={Slug}='${slug}'`,
+    { headers:{ Authorization:`Bearer ${process.env.AIRTABLE_TOKEN}` } }
+  );
+
+  const d = await r.json();
+  const p = d.records[0].fields;
+
+  res.json({ name:p.Name, description:p.Description });
+}
 
