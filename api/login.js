2cd2bcc8f23a8344c6f2d69848c18ca6accb0b65
Andrea-FEVE <85485500+Andrea-FEVE@users.noreply.github.com>
2025-12-23 21:59:15 +0100

    Update login.js

--- api/login.js
+++ api/login.js
@@ -1,1 +1,18 @@
+import crypto from "crypto";
+
+export default function handler(req, res) {
+  const hash = crypto.createHash("sha256")
+    .update(req.body.password)
+    .digest("hex");
+
+  if (hash !== process.env.SITE_PASSWORD_HASH)
+    return res.status(401).end();
+
+  res.setHeader(
+    "Set-Cookie",
+    "auth=1; HttpOnly; Path=/; Max-Age=86400; SameSite=Strict"
+  );
+
+  res.end();
+}
 
